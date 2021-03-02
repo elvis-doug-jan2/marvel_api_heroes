7a0ad3f44d64f34f5bb0e68667a9a07286e55c04
Elvis Douglas Janegitz <edjelvis@gmail.com>
2021-03-01 21:50:27 -0300

    Adicionando arquivo de configuração com as variáveis e corrigindo configuração do docker-compose para ambiente de dev

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import config from './config/env.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
-  app.setGlobalPrefix(process.env.GLOBAL_PREFIX)
+  app.setGlobalPrefix(config.prefix)
 
   const options = new DocumentBuilder()
     .setTitle('Marvel Herores API')
@@ -15,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(process.env.PORT)
+  await app.listen(config.port)
 }
 
 bootstrap()
